0f16504ee5ba5828862472eb11de28eae977a85b
Till Arnold <throwable42@gmail.com>
2021-05-15 11:51:22 +0200

    Try compressing and bundling

---
+++ dist.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(realpath "$SCRIPT_DIR")"
+
+
+function report() {
+	echo "[$SCRIPT_NAME]" "$@"
+}
+
+
+cd "$SCRIPT_DIR"
+rm -rf dist
+mkdir dist
+
+cp -r img dist/
+cp -r css dist/
+mkdir dist/js
+npm run dist-main-js
+cp favicon.ico icon.png index.html site.webmanifest sw.js dist/
+
+report "done"
